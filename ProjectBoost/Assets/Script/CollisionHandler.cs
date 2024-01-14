ed06a118df0a0bea9724e70a0dc377310962a216
UHANKNAG <92603894+UHANKNAG@users.noreply.github.com>
2024-01-14 20:20:22 +0900

    create level selector

--- ProjectBoost/Assets/Script/CollisionHandler.cs
+++ ProjectBoost/Assets/Script/CollisionHandler.cs
@@ -111,6 +111,10 @@ public class CollisionHandler : MonoBehaviour
 
         int nextSceneIndex = SetSceneIndex();
         StartLoadNextScene(waitTime, nextSceneIndex);
+
+        if (nextSceneIndex > PlayerPrefs.GetInt("levelAt")) {
+            PlayerPrefs.SetInt("levelAt", nextSceneIndex);
+        }
     }
 
     private void FinishEffect()
